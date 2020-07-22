228a061a1e5e5b208a8222783017f291f28c9f12
Sharad Kumar <sharad.kumar@byjus.com>
2020-07-22 08:15:45 +0530

    vs setup

---
+++ Exercise/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercise {
+    class MainClass {
+        public static void Main(string[] args) {
+            
+            int[] unsortedArray = { 3, 2, 1, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Un sorted array : ");
+
+            foreach (int item in unsortedArray) {
+                Console.WriteLine(item);
+            }
+            bool isSorted = false;
+            while (!isSorted) {
+                isSorted = true;
+                for (int i=0;i< unsortedArray.Length-1; i++) {
+                    if(unsortedArray[i]> unsortedArray[i + 1]) {
+                        int temp = unsortedArray[i];
+                        unsortedArray[i] = unsortedArray[i + 1];
+                        unsortedArray[i + 1] = temp;
+                        isSorted = false;
+                    }
+                }
+            }
+
+            Console.WriteLine(" sorted array : ");
+
+            foreach (int item in unsortedArray) {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
