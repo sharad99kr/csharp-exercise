8418d8197fc5e411fee9f44fa5dc5ee7fba4346d
Sharad Kumar <sharad99kr@gmail.com>
2020-07-29 07:05:13 +0530

    added linear search

---
+++ Exercise/LinearSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Exercise {
+    public class LinearSearch {
+        public static void Main() {
+            int[] arr = { 4, 9, 5, 1, 6, 2, 8, 10, 3, 12, 15, 17, 19, 11, 13, 16, 14 };
+            Console.Write("Un sorted array : ");
+
+            int key = 6;
+            foreach (int item in arr) {
+                Console.Write(item + " ");
+            }
+            int index = 0;
+            foreach (int item in arr) {
+                
+                if (key == item) {
+                    Console.Write("item found at index : "+index);
+                    break;
+                }
+                index++;
+            }
+
+        }
+    }
+}
